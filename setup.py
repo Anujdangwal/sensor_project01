3a2e8b21d116b73ff78338b5e19fd6aa5d7b9f76
Anujdangwal <anujdangwal20@gmail.com>
2025-03-20 02:52:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup , find_packages
+
+from typing import List
+
+HYPEN_E_DOT = "-e."
+def get_requirements(file_path:str)->List[str]:
+    requirements =[]
+    with open(file_path) as file_object:
+        reuirements = file_object.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+    if HYPEN_E_DOT in get_requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name ="fault detection",
+    version = "0.0.1",
+    author ="Anuj",
+    author_mail = "anujdangwal202gmail.com",
+    install_packages = get_requirements("requirements.txt")
+)
